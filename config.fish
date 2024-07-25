e604242ff6c6e4b0269543c6b36e32b9239ac36c
Iuro Nascimento <iurobpn@proton.me>
2024-07-24 23:28:20 -0300

    fix auto attach zellij configure zellij creation of new panes

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-source $HOME/.cargo/env.fish
+source "$HOME/.cargo/env.fish"
 
 if status is-interactive
     theme_gruvbox dark hard
@@ -9,28 +9,34 @@ end
 fzf --fish | source
 
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/gagarin/sdd/anaconda3/bin/conda
-    eval /home/gagarin/sdd/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
-        . "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
+set -gx PATH $PATH /home/gagarin/.local/bin
+set -xg HOST $(hostname)
+if [ $HOST != "dplagueis" ]
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    if test -f /home/gagarin/sdd/anaconda3/bin/conda
+        eval /home/gagarin/sdd/anaconda3/bin/conda "shell.fish" "hook" $argv | source
     else
-        set -x PATH "/home/gagarin/sdd/anaconda3/bin" $PATH
+        if test -f "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
+            . "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH "/home/gagarin/sdd/anaconda3/bin" $PATH
+        end
     end
+    # <<< conda initialize <<<
+else
+    echo "host is $HOST"
 end
-# <<< conda initialize <<<
 
 source /opt/ros/noetic/share/rosbash/rosfish
 bass source $HOME/catkin_ws/devel/setup.bash
 set -gx CONAN_PROVIDER /home/gagarin/git/cmake-conan/conan_provider.cmake
 set -gx CMAKE_PREFIX_PATH "$CMAKE_PREFIX_PATH:/usr/local/lib/cmake/absl:/usr/local/share/Tracy"
+set xg ZELLIJ_AUTO_ATTACH true
 
-set -gx ZELLIJ_AUTO_START true
-
-if status is-interactive
-    zellij attach -c
-    # eval (zellij setup --generate-auto-start fish | string collect)
+if set -q ZELLIJ
+else
+  zellij attach -c
 end
+
 
