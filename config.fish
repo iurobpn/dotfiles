361ba7f90bac3a5acaf45b1f29411e87f6010678
Iuro Nascimento <iurobpn@proton.me>
2024-09-02 09:36:47 -0300

    add gtags data, add vim-easy-align and mini-nvim

--- config.fish
+++ config.fish
@@ -1,12 +1,12 @@
-set -xg TODO_PATH $HOME/sync/tlua
 if status is-interactive
     theme_gruvbox dark hard
-    source ~/git/dotfiles/.fish_aliases
+    source $HOME/git/dotfiles/.fish_aliases
     set -gx EDITOR nvim
     fzf --fish | source
     source "$HOME/.cargo/env.fish"
 
-    set -gx PATH /home/gagarin/.rbenv/versions/3.3.4/bin $PATH /home/gagarin/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts
+    set -axg PATH $HOME/.rbenv/versions/3.3.4/bin $HOME/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts
+
     set -xg HOST $(hostname)
 
     if [ $HOST = "dplagueis" ]
@@ -16,14 +16,14 @@ if status is-interactive
 
         # ---- conda config init
         # !! Contents within this block are managed by 'conda init' !!
-        if test -f /home/gagarin/sdd/anaconda3/bin/conda
-            eval /home/gagarin/sdd/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+        if test -f $HOME/sdd/anaconda3/bin/conda
+            eval $HOME/sdd/anaconda3/bin/conda "shell.fish" "hook" $argv | source
         else
 
-            if test -f "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
-                . "/home/gagarin/sdd/anaconda3/etc/fish/conf.d/conda.fish"
+            if test -f "$HOME/sdd/anaconda3/etc/fish/conf.d/conda.fish"
+                . "$HOME/sdd/anaconda3/etc/fish/conf.d/conda.fish"
             else
-                set -x PATH "/home/gagarin/sdd/anaconda3/bin" $PATH
+                set -x PATH "$HOME/sdd/anaconda3/bin" $PATH
             end
         end  # ---- conda config end
     end # ---- host check end
@@ -33,12 +33,11 @@ if status is-interactive
     source /opt/ros/noetic/share/rosbash/rosfish
     bass source $HOME/catkin_ws/devel/setup.bash
 
-    set -gx CONAN_PROVIDER /home/gagarin/git/cmake-conan/conan_provider.cmake
+    set -gx CONAN_PROVIDER $HOME/git/cmake-conan/conan_provider.cmake
     set -gx CMAKE_PREFIX_PATH "$CMAKE_PREFIX_PATH:/usr/local/lib/cmake/absl:/usr/local/share/Tracy"
 
     set -gx ZELLIJ_AUTO_ATTACH true
-    if set -q ZELLIJ
-    else
+    if not set -q ZELLIJ
         zellij delete-all-sessions -y
         set N_SESSIONS $(zellij list-sessions | grep -v EXITED | wc -l)
         if test $N_SESSIONS -eq 0
@@ -52,14 +51,14 @@ if status is-interactive
     if [ $HOST = "dplagueis" ]
         eval "$(luarocks path --bin | sed 's/export \(.*\)/set -xg \1/g' | sed 's/=/ /g')"
     else
-        eval "$(/home/gagarin/.luarocks51/bin/luarocks path --bin | sed 's/export \(.*\)/set -xg \1/g' | sed 's/=/ /g')"
+        eval "$($HOME/.luarocks51/bin/luarocks path --bin | sed 's/export \(.*\)/set -xg \1/g' | sed 's/=/ /g')"
     end
+
     set -xg FZF_DEFAULT_OPTS " --multi --info=inline --preview 'bat --color=always --style=header,grid --line-range :500 {}' --preview-window=right:60%:wrap"
     set -xg FZF_DEFAULT_COMMAND 'fd . --type f --hidden --follow --exclude .git --exclude .gtags'
     set -xg LUA_PATH "$LUA_PATH;$HOME/git/scripts/lua/?.lua;$HOME/git/scripts/lua/?/init.lua;$HOME/git/scripts/lua/?.lua"
-    source $HOME/git/scripts/fzf.fish
+
+    source $HOME/git/scripts/scripts.fish
+    set -xg TEXMFHOME '$HOME/.texmf'
 end
-
-
-# set -xg TEXMFCNF '~/.texmf'
-set -xg TEXMFHOME '~/.texmf'
+set -xg PATH $PATH $HOME/git/scripts/treesitter/node_modules/.bin
