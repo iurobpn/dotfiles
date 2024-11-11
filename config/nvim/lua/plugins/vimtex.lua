96869014cb825b6902a8269b51c2cac53c452205
Iuro Nascimento <iurobpn@proton.me>
2024-11-11 13:17:18 -0300

    change vitmtex config to not jump to qf automatically

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -14,10 +14,13 @@ return {
         vim.g.vimtex_mappings_enabled=1
         vim.g.vimtex_fold_enabled=1
         vim.g.vimtex_syntax_conceal_disable=0
+
         vim.g.vimtex_quickfix_enabled=1
         vim.g.vimtex_quickfix_open_on_warning = 0
         vim.g.vimtex_quickfix_ignore_filters = { 'Warning', 'Package', 'Missing character'}
         vim.g.vimtex_quickfix_mode = 1
+        vim.g.vimtex_quickfix_autojump = 0
+
         vim.g.vimtex_view_general_viewer = 'okular'
         vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
         vim.g.vimtex_complete_enabled=1
