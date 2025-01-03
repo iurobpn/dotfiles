87dad342a99c06a3ff61af9225b3fe6f5003d478
Iuro Nascimento <iurobpn@proton.me>
2025-01-03 12:45:20 -0300

    configure dashboard from snacks

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -3,7 +3,41 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        dashboard = { enabled = false },
+        dashboard = { 
+            enabled = true,
+            sections = {
+                { section = "header" },
+                {
+                    pane=2,
+                    title = "Reminders:\n",
+                },
+                -- {
+                --     pane = 2,
+                --     section = "terminal",
+                --     cmd = "colorscript -e square",
+                --     height = 5,
+                --     padding = 1,
+                -- },
+                { section = "keys", gap = 1, padding = 1 },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = function()
+                        return Snacks.git.get_root() ~= nil
+                    end,
+                    cmd = "git status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
+                { section = "startup" },
+            },
+        },
         indent = { enabled = true },
         input = { enabled = false },
         notifier = { enabled = false },
