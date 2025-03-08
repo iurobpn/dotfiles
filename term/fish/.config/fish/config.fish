904f4015c63d7f25a6fc57cac00cba9f5345cb77
Iuro Nascimento <iurobpn@proton.me>
2025-03-08 20:09:05 -0300

    many fish config updates

--- term/fish/.config/fish/config.fish
+++ term/fish/.config/fish/config.fish
@@ -16,6 +16,8 @@ if test -f $CONDA_PATH/share/fish/vendor_completions.d/papis.fish
 end
 
 if status is-interactive
+    # Preview file content using bat (https://github.com/sharkdp/bat)
+    # bind ctrl-shift-
     theme_gruvbox dark hard
     fish_vi_key_bindings
     source (status dirname)/.fish_aliases
@@ -29,8 +31,9 @@ if status is-interactive
 
     # set -axg PATH $HOME/.rbenv/versions/3.3.4/bin $HOME/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts
     set -Ux FORGIT_INSTALL_DIR ~/git/forgit
-    fish_add_path --prepend $DOT/bin HOME/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts $HOME/git/scripts/treesitter/node_modules/.bin $FORGIT_INSTALL_DIR/bin /opt/lualanguageserver/bin ~/go/bin/
-
+    fish_add_path --prepend $DOT/bin HOME/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts $HOME/git/scripts/treesitter/node_modules/.bin $FORGIT_INSTALL_DIR/bin /opt/lualanguageserver/bin ~/go/bin/ $HOME/.local/share/gem/ruby/3.2.0/bin
+    set -Uax GZ_SIM_RESOURCE_PATH $HOME/.gazebo/models
+    # GZ_SIM_SYSTEM_PLUGIN_PATH
 
     set -xg HOST $(hostname)
 
@@ -77,11 +80,34 @@ if status is-interactive
     set -xga FZF_DEFAULT_OPTS "--color=fg:#ebdbb2,bg:#282828,hl:#b16286 --color=fg+:#689d6a,bg+:#32302f,hl+:#d3869b --color=info:#d65d0e,prompt:#458588,pointer:#fe8019 --color=marker:#8ec07c,spinner:#cc241d,header:#fabd2f --reverse --multi --info=inline"
     # --preview 'bat --color=always --style=header,grid --line-range :500 {}' --preview-window=right:60%:wrap"
     set -xg FZF_DEFAULT_COMMAND 'fd . --type f --hidden --follow --exclude .git --exclude .gtags'
+    set -Ux FZF_CTRL_T_OPTS "
+    --walker-skip .git,node_modules,target
+    --preview 'bat -n --color=always {}'
+    --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+    # CTRL-Y to copy the command into clipboard using pbcopy
+    set -Ux FZF_CTRL_R_OPTS "
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --color header:italic
+    --header 'Press CTRL-Y to copy command into clipboard'"
+
+    # Print tree structure in the preview window
+    set -Ux FZF_ALT_C_OPTS "
+    --walker-skip .git,node_modules,target
+    --preview 'tree -C {}'"
+
+    bind -M insert \ck 'fd -td | fzf --preview "tree -C {}"'
+    bind -M insert \cf 'fd -tf | fzf'
+
+
+    # fzf_configure_bindings --git_log=\ch
+
     set -Ux LUA_PATH "$LUA_PATH;$HOME/git/scripts/lua/?.lua;$HOME/git/scripts/lua/?/init.lua;$HOME/git/scripts/lua/?.lua"
 
     if [ -f $HOME/git/scripts/scripts.fish ]
         source $HOME/git/scripts/scripts.fish
     end
+
     set -xg TEXMFHOME '$HOME/.texmf'
     fish_add_path -p /usr/local/texlive/2024/bin/x86_64-linux
     # set -xag MANPATH /usr/local/texlive/2024/texmf-dist/doc/man
@@ -105,10 +131,14 @@ if status is-interactive
         fish_add_path --prepend "$PNPM_HOME"
     end
     # pnpm end
+    function pimp
+        papis bibtex read $argv import
+    end
+
 end
-# if test -f ~/.config/fish/git-forgit.fish
-. ~/.config/fish/git-forgit.fish
-# end
+if test -f ~/.config/fish/git-forgit.fish
+    . ~/.config/fish/git-forgit.fish
+end
 # echo 'non-interactive fish'
 
 if [ -f $DOT/gruvbox/gruvbox.fish ]
@@ -169,7 +199,7 @@ end
 # pnpm
 set -gx PNPM_HOME "/home/gagarin/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 set -Ux SOFT_SERVE_DATA_PATH "$HOME/hds/hdd/data/soft-serve"
@@ -188,3 +218,16 @@ set -gx FZF_BIBTEX_CACHEDIR ~/.bibtex-fzf/cache
 set -gx FZF_BIBTEX_SOURCES ~/.bibtex-fzf/bib
 
 set -gx WEZ_FONT_SIZE 12
+
+set -x PATH /home/gagarin/perl5/bin $PATH 2>/dev/null;
+set -q PERL5LIB; and set -x PERL5LIB /home/gagarin/perl5/lib/perl5:$PERL5LIB;
+set -q PERL5LIB; or set -x PERL5LIB /home/gagarin/perl5/lib/perl5;
+set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /home/gagarin/perl5:$PERL_LOCAL_LIB_ROOT;
+set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /home/gagarin/perl5;
+set -x PERL_MB_OPT --install_base\ \"/home/gagarin/perl5\";
+set -x PERL_MM_OPT INSTALL_BASE=/home/gagarin/perl5;
+
+set -Ux FONTCONFIG_PATH /etc/fonts
+set -Ux FONTCONFIG_FILE /etc/fonts/fonts.conf
+set -Ux GZ_SIM_SYSTEM_PLUGIN_PATH "/opt/ros/jazzy/opt/gz_gui_vendor/lib/gz-gui-8/plugins:/opt/ros/jazzy/opt/gz_sim_vendor/lib/gz-sim-8/plugins/gui"
+
