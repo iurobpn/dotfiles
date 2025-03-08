a10009837aada7905d6d132981a1757152f33f72
Iuro Nascimento <iurobpn@proton.me>
2025-03-08 20:05:43 -0300

    config browser-bookmarks to work with buku

--- config/nvim/lua/plugins/browser-bookmarks.lua
+++ config/nvim/lua/plugins/browser-bookmarks.lua
@@ -8,11 +8,11 @@ return {
         url_open_command = 'xdg-open',
         -- config_dir = '~/.config/buku',
     },
-    -- dependencies = {
-    --   -- Only if your selected browser is Firefox, Waterfox or buku
-    --   'kkharji/sqlite.lua',
+    dependencies = {
+      -- Only if your selected browser is Firefox, Waterfox or buku
+      'kkharji/sqlite.lua',
     --
     --   -- Only if you're using the Telescope extension
     --   'nvim-telescope/telescope.nvim',
-    -- }
+    }
 }
