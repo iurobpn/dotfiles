8e4a10fa29ae35e48a5aeecf2bf3b8dab5d2c5e1
Iuro Nascimento <iurobpn@proton.me>
2025-03-25 19:43:06 -0300

    update ansible for initial install when reinstalling

---
+++ ansible/config.rockspec
@@ -0,0 +1,23 @@
+rockspec_format = "3.0"
+
+package = "example"
+
+version = "dev-1"
+
+source = {
+  url = "..."
+}
+
+dependencies = {
+    "lua-cjson",
+    "dkjson",
+    "inspect",
+    "lsqlite3",
+    "lua-cjson",
+    "luafilesystem",
+    "luasec",
+    "luasocket",
+    "luasql-sqlite3",
+    "lustache",
+    "sqlite.lua"
+}
---
+++ ansible/init-install.sh
@@ -0,0 +1,26 @@
+sudo apt install python3 python-is-python3 ansible syncthing openssh-server
+# get keepassxc working and access github and download the ansible playbook
+# add keypassxc ppa and install it
+sudo apt install keepassxc
+# get db from syncthing
+# sign in to github
+# create ssh key pair
+
+# send ssh key to github
+xclip -sel clip ~/.ssh/id_rsa.pub
+
+# clone dotfiles
+cd ~/git/dotfiles/ansible
+ansible-playbook -i inventory.ini playbook.yml
+
+
+pip and npm
+neovim
+
+npm install -g neovim
+# instasll ruby
+gem install neovim
+# install pearl
+# install provider
+rg fd
+"Neovim::Ext" cpan module
