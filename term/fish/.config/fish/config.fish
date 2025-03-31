5f5f43006190152b678bb0f8fca58317d10cabc3
Iuro Nascimento <iurobpn@proton.me>
2025-03-31 09:39:20 -0300

    fiz issues

--- term/fish/.config/fish/config.fish
+++ term/fish/.config/fish/config.fish
@@ -1,22 +1,18 @@
 if status is-interactive
-	theme_gruvbox dark hard
+	# theme_gruvbox dark hard
 	fish_vi_key_bindings
-	source (status dirname)/.fish_aliases
+    source (status dirname)/.fish_aliases
 	set -Ux EDITOR nvim
 	fzf --fish | source
 	set -xg DOT $HOME/git/dotfiles
 
-	# set -axg PATH $HOME/.rbenv/versions/3.3.4/bin $HOME/.local/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts
 	fish_add_path --prepend $DOT/bin HOME/.local/bin $HOME/.venv/bin /usr/local/go/bin $HOME/git/scripts/lua $HOME/git/scripts $HOME/git/scripts/treesitter/node_modules/.bin
-
 
 	set -xg HOST $(hostname)
 
 	eval "$(luarocks path --bin | sed 's/export \(.*\)/set -xU \1/g' | sed 's/=/ /g')"
-	#eval "$($HOME/.luarocks51/bin/luarocks path --bin | sed 's/export \(.*\)/set -xg \1/g' | sed 's/=/ /g')"
 
 	set -xg FZF_DEFAULT_OPTS "--reverse --multi --info=inline"
-	# --preview 'bat --color=always --style=header,grid --line-range :500 {}' --preview-window=right:60%:wrap"
 	set -xg FZF_DEFAULT_COMMAND 'fd . --type f --hidden --follow --exclude .git --exclude .gtags'
 	set -Ux LUA_PATH "$LUA_PATH;$HOME/git/scripts/lua/?.lua;$HOME/git/scripts/lua/?/init.lua;$HOME/git/scripts/lua/?.lua"
 
@@ -31,7 +27,7 @@ if status is-interactive
 end
 # echo 'non-interactive fish'
 
-source $DOT/gruvbox/gruvbox.fish
+# source $DOT/gruvbox/gruvbox.fish
 
 set -gx tide_character_icon           ∫
 set -gx tide_character_vi_icon_default ξ
@@ -57,12 +53,4 @@ set -gx tide_pwd_color_anchors        $bright_blue
 set -xg fzf_preview_command 'bat --style=numbers --color=always --theme=gruvbox-dark --highlight-line=$(echo {} | cut -d: -f2) $(echo {} | cut -d: -f1)'
 
 zoxide init fish | source
-
-#if not set -q TMUX
-	#tmux new-session -d -s base
-	#set -g TMUX (tmux new-session -d -s base)
-	#tmux attach -t base
-#else
-	#tmux attac
-#end
-# starship init fish | source
+    
