574d4ebda5eedf0ee66dfad270f4d4279a972217
Iuro Nascimento <iurobpn@proton.me>
2025-04-06 08:03:30 -0300

    fix bootstrap setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 # vi: ft=bash
 
-echo "##### Symlinkng dotfile configurations #####"
+set -e
+echo "##### Symlinking dotfile configurations #####"
 echo ""
 
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+HOME_DIR=/root
+
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+echo "HOME_DIR=$HOME_DIR"
 # userspace application configurations
-mv $HOME/.bashrc $HOME/.bashrc.bkp
-stow -v -R -t $HOME/ home
+# mv $HOME_DIR/.bashrc $HOME_DIR/.bashrc.bkp 2> /dev/null || echo "$HOME/.bashrc already backed up"
+echo '. $HOME/.bashrc_base' >>  $HOME_DIR/.bashrc
+cd $SCRIPT_DIR/../
+stow -v -R -t $HOME_DIR/ home
 # ssh and other configs
-#stow /etc etc
+# stow /etc etc
+cd -
 
 echo ""
 echo "############   ALL DONE   ################"
