b41d7dd07fdd36dbd004076e0fb2276c1aad120a
Iuro Nascimento <iurobpn@proton.me>
2025-04-15 00:30:16 -0300

    fixing ansible bootstrap dotfiles install

--- ansible/init-install.sh
+++ ansible/init-install.sh
@@ -1,27 +1,28 @@
 sudo apt install python3 python-is-python3 ansible syncthing openssh-server
 # get keepassxc working and access github and download the ansible playbook
 # add keypassxc ppa and install it
-sudo apt install keepassxc
+#sudo apt install keepassxc
 # get db from syncthing
 # sign in to github
 # create ssh key pair
 
 # send ssh key to github
-xclip -sel clip ~/.ssh/id_rsa.pub
+#xclip -sel clip ~/.ssh/id_rsa.pub
 
 # clone dotfiles
-cd ~/git/dotfiles/ansible
-ansible-playbook -i inventory.ini playbook.yml
+#cd ~/git/dotfiles/ansible
+#ansible-playbook -i inventory.ini playbook.yml
 
-ssh-keygen -t ed25519 -C "$(whoami)@$(uname -n)-$(date -I)-install"
 
-pip and npm
-neovim
+#ssh-keygen -t ed25519 -C "$(whoami)@$(uname -n)-$(date -I)-install"
 
-npm install -g neovim
+#pip and npm
+#neovim
+
+#npm install -g neovim
 # instasll ruby
-gem install neovim
+#gem install neovim
 # install pearl
 # install provider
-rg fd
-"Neovim::Ext" cpan module
+#rg fd
+#"Neovim::Ext" cpan module
