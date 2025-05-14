cccad27a0f990a2765552ac8ca9073f0d085a16a
Iuro Nascimento <iurobpn@proton.me>
2025-05-14 18:20:12 -0300

    add gh litee plugin to neovim

--- home/.config/nvim/lua/config/init.lua
+++ home/.config/nvim/lua/config/init.lua
@@ -47,3 +47,4 @@ end
 local colors = vim.g.gruvbox_palette
 vim.api.nvim_set_hl(0, "MarkviewCheckboxUnchecked", { fg = colors.light1 })
 -- vim.api.nvim_set_hl(0, "MarkviewPalette1Fg", { fg = colors.light4 })
+require'config.gh'
