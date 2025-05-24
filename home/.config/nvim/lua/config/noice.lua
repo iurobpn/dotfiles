271af5a35c7e491d8369a1673bd900bd29e2debc
Iuro Nascimento <iurobpn@proton.me>
2025-05-24 13:12:29 -0300

    change noice configuration

--- home/.config/nvim/lua/config/noice.lua
+++ home/.config/nvim/lua/config/noice.lua
@@ -1,18 +1,53 @@
 require("noice").setup({
-    lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    cmdline = {
+        format = {
+            cmdline = { icon = ":" },
+            search_down = { icon = "/" },
+            search_up = { icon = "?" },
+            filter = { icon = "$" },
+            lua = { icon = "☾" },
+            help = { icon = "?" },
         },
     },
-    -- you can enable a preset for easier configuration
-    presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
+    -- lsp = {
+    --     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    --     override = {
+    --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    --         ["vim.lsp.util.stylize_markdown"] = true,
+    --         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    --     },
+    -- },
+    -- cmdline = {
+        -- enabled = true, -- enables the Noice cmdline UI
+        -- view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+    -- },
+        -- opts = {}, -- global options for the cmdline. See section on views
+        ---@type table<string, CmdlineFormat>
+        -- format = {
+            -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+         --    view = {
+         --        cmdline_popup = "cmdline",    
+         --    },
+            -- opts: any options passed to the view
+            -- icon_hl_group: optional hl_group for the icon
+            -- title: set to anything or empty string to hide
+         --    cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+         --    search_down = false, --{ kind = "search", pattern = "^/", icon = " /", lang = "regex" },
+         --    search_up = false, --{ kind = "search", pattern = "^%?", icon = " ?", lang = "regex" },
+         --    filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+         --    lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+         --    help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+         --    input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+            -- lua = false, -- to disable a format, set to `false`
+        -- },
+    -- },    -- you can enable a preset for easier configuration
+    -- views = {
+    --     cmdline = {
+    --         border = {
+    --             style = "none",
+    --             border = "none",
+    --      --        padding = { 0, 0 },
+    --         },
+    --     },
+    -- },
 })
--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -4,6 +4,13 @@ return {
     event = "VeryLazy",
     opts = {
         -- add any options here
+        presets = {
+            bottom_search = false, -- use a classic bottom cmdline for search
+            command_palette = false, -- position the cmdline and popupmenu together
+            long_message_to_split = false, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
     },
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
