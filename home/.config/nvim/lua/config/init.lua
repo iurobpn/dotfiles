f4a35d1aba2336687516c3576debfe1305b0c3a6
Iuro Nascimento <iurobpn@proton.me>
2025-05-29 11:53:02 -0300

    add tasks nvim config

--- home/.config/nvim/lua/config/init.lua
+++ home/.config/nvim/lua/config/init.lua
@@ -34,36 +34,18 @@ require'config.spelunk'
 require"config.various-textobjs"
 
 require"config.gruvbox"
+require"config.tasks"
 
-if vim.g.debug_dev then
-        tasks_dir = os.geenv("HOME") .. "/git/my/home/dev/tasks.nvim"
-        vim.cmd("set rtp+=" .. tasks_dir)
-        vim.cmd("set rtp+=" .. tasks_dir .. "/lua")
-        package.path = tasks_dir .. "/lua/?/init.lua" ..
-                       ";" .. tasks_dir .. "/lua/?/?.lua" ..
-                        ";" .. package.path
-end
+-- if vim.g.debug_dev then
+--         tasks_dir = os.geenv("HOME") .. "/git/my/home/dev/tasks.nvim"
+--         vim.cmd("set rtp+=" .. tasks_dir)
+--         vim.cmd("set rtp+=" .. tasks_dir .. "/lua")
+--         package.path = tasks_dir .. "/lua/?/init.lua" ..
+--                        ";" .. tasks_dir .. "/lua/?/?.lua" ..
+--                         ";" .. package.path
+-- end
 
 -- local colors = vim.g.gruvbox_palette
 -- vim.api.nvim_set_hl(0, "MarkviewCheckboxUnchecked", { fg = colors.light1 })
 -- vim.api.nvim_set_hl(0, "MarkviewPalette1Fg", { fg = colors.light4 })
 
-local function open_daily_template()
-    local home = os.getenv("HOME")
-    local dir = os.getenv("HOME") .. "/git/my/home/pkm/daily"
-    local fname = os.date("%Y-%m-%d") .. ".md"
-    --check if fname exists in dir
-    local full_path = dir .. "/" .. fname
-    
-    if not require'utils.fs'.file_exists(full_path) then
-        local tmpl_dir =  home .. "/git/my/home/dotfiles/home/.config/nvim/templates"
-        vim.fn.mkdir(dir, "p")
-        vim.cmd('edit ' .. full_path)
-
-        vim.cmd('TemplIns ' .. tmpl_dir .. '/daily.tpl')
-        vim.cmd('write')
-    else
-        vim.cmd('edit ' .. full_path)
-    end
-end
-vim.api.nvim_create_user_command("Daily", open_daily_template, { desc = "Open daily template" })
---
+++ home/.config/nvim/lua/config/tasks.lua
@@ -0,0 +1,25 @@
+local path = vim.fn.stdpath("data") .. "/ggn/tasks.nvim"
+-- vim.cmd('set rtp^=' .. home .. "/.local/share/nvim/site,")
+vim.opt.rtp:prepend(path)
+
+_G.Tasks = require'tasks'
+
+local function open_daily_template()
+    local home = os.getenv("HOME")
+    local dir = os.getenv("HOME") .. "/git/my/home/pkm/daily"
+    local fname = os.date("%Y-%m-%d") .. ".md"
+    --check if fname exists in dir
+    local full_path = dir .. "/" .. fname
+    
+    if not require'utils.fs'.file_exists(full_path) then
+        local tmpl_dir =  home .. "/git/my/home/dotfiles/home/.config/nvim/templates"
+        vim.fn.mkdir(dir, "p")
+        vim.cmd('edit ' .. full_path)
+
+        vim.cmd('TemplIns ' .. tmpl_dir .. '/daily.tpl')
+        vim.cmd('write')
+    else
+        vim.cmd('edit ' .. full_path)
+    end
+end
+vim.api.nvim_create_user_command("Daily", open_daily_template, { desc = "Open daily template" })
