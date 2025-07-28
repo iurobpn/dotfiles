03cf39dc1b902a4dd191286c1716633a9b75ce83
Iuro Nascimento <iurobpn@proton.me>
2025-07-28 11:39:51 -0300

    add auto clone for katu and tasks nvim

--- home/.config/nvim/lua/config/katu.lua
+++ home/.config/nvim/lua/config/katu.lua
@@ -1,6 +1,9 @@
 local isdev = os.getenv("ISDEV") or "0"
 if isdev=="0" then
     local path = vim.fn.stdpath("data") .. "/ggn/katu"
+    if not vim.uv.fs_stat(path) then
+        os.execute("git clone https://github.com/iurobpn/katu.git " .. path)
+    end
     vim.opt.rtp:prepend(path)
 end
 _G.Katu = require('katu')
--- home/.config/nvim/lua/config/tasks.lua
+++ home/.config/nvim/lua/config/tasks.lua
@@ -1,6 +1,9 @@
 local isdev = os.getenv("ISDEV") or "0"
 if isdev=="0" then
     local path = vim.fn.stdpath("data") .. "/ggn/tasks.nvim"
+    if not vim.uv.fs_stat(path) then
+        os.execute("git clone https://github.com/iurobpn/tasks.nvim.git " .. path)
+    end
     vim.opt.rtp:prepend(path)
 end
 -- vim.cmd('set rtp^=' .. path)
